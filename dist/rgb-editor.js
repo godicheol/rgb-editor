caeadf5f085574ff6ed306ea7b7f0939d26d19d1
2cheol <godicheol@gmail.com>
2022-06-21 21:23:59 +0900

    1

--- dist/rgb-editor.js
+++ dist/rgb-editor.js
@@ -15,7 +15,10 @@
             canvas.height = imageHeight;
             ctx.drawImage(image, 0, 0);
             imageData = ctx.getImageData(0, 0, imageWidth, imageHeight);
-            return cb(null, imageData);
+            return cb(null, imageData, {
+                width: imageWidth,
+                height: imageHeight,
+            });
         }
         image.onerror = function() {
             return cb(new Error("Load error"));
@@ -23,103 +26,58 @@
         image.src = src;
     }
 
-    exports.get = function(src, cb) {
-        var getImageData = this.getImageData;
-        var i, j, imageWidth, imageHeight, idx, output = [];
-        var r, g, b, a;
-
-        getImageData(src, function(err, imageData) {
-            if (err) {
-                return cb(err);
-            }
-            imageWidth = imageData.width;
-            imageHeight = imageData.height;
-            for (j = 0; j < imageHeight; j++) {
-                for (i = 0; i < imageWidth; i += 4) {
-                    idx = (j * imageWidth) + i;
-                    r = imageData.data[idx];
-                    g = imageData.data[idx+1];
-                    b = imageData.data[idx+2];
-                    a = imageData.data[idx+3];
-                    output.push([r,g,b,a]);
-                }
-            }
-            return cb(null, output);
-        });
+    exports.copyImageData = function(imageData) {
+        var dataArray = new Uint8ClampedArray(imageData.data);
+        return new ImageData(
+            dataArray,
+            imageData.width,
+            imageData.height
+        );
     }
 
     exports.edit = function(src, filter, cb) {
         var getImageData = this.getImageData;
-        var i, j, imageWidth, imageHeight, res, idx;
-        var r, g, b, a;
-
+        var setFilter = this.setFilter;
         if (typeof(filter) !== "function") {
             filter = function(r,g,b,a) {
                 return [r,g,b,a];
             }
         }
-
         getImageData(src, function(err, imageData) {
             if (err) {
                 return cb(err);
             }
-            imageWidth = imageData.width;
-            imageHeight = imageData.height;
-            for (j = 0; j < imageHeight; j++) {
-                for (i = 0; i < imageWidth; i++) {
-                    idx = (j*4)*imageWidth+(i*4);
-                    r = imageData.data[idx];
-                    g = imageData.data[idx+1];
-                    b = imageData.data[idx+2];
-                    a = imageData.data[idx+3];
-
-                    res = filter(r,g,b,a);
-
-                    imageData.data[idx] = res[0];
-                    imageData.data[idx+1] = res[1];
-                    imageData.data[idx+2] = res[2];
-                    imageData.data[idx+3] = res[3];
-                }
-            }
-
+            setFilter(imageData, filter);
             return cb(null, imageData);
         });
     }
 
-    exports.filter = function(data, filter) {
-        var i, j, imageWidth, imageHeight, imageData, dataArray, res, idx;
+    exports.setFilter = function(imageData, filter) {
+        var i, j, xlen, ylen, imageData, res, idx;
         var r, g, b, a;
 
         if (typeof(filter) !== "function") {
-            filter = function(r,g,b,a) {
+            filter = function(r,g,b,a,x,y) {
                 return [r,g,b,a];
             }
         }
 
-        dataArray = new Uint8ClampedArray(data.data);
-        imageData = new ImageData(
-            dataArray,
-            data.width,
-            data.height
-        );
-        
-        imageWidth = imageData.width;
-        imageHeight = imageData.height;
-        for (j = 0; j < imageHeight; j++) {
-            for (i = 0; i < imageWidth; i++) {
-                idx = (j*4)*imageWidth+(i*4);
+        xlen = imageData.width;
+        ylen = imageData.height;
+        for (j = 0; j < ylen; j++) {
+            for (i = 0; i < xlen; i++) {
+                idx = (j*xlen*4)+(i*4);
                 r = imageData.data[idx];
                 g = imageData.data[idx+1];
                 b = imageData.data[idx+2];
                 a = imageData.data[idx+3];
-                res = filter(r,g,b,a);
+                res = filter(r,g,b,a,i,j);
                 imageData.data[idx] = res[0];
                 imageData.data[idx+1] = res[1];
                 imageData.data[idx+2] = res[2];
                 imageData.data[idx+3] = res[3];
             }
         }
-
         return imageData;
     }
 
